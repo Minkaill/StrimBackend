0b4246dd965ad06722d6a25f97f2e9c8bb7e9cd5
Ross <rabik95@bk.ru>
2022-08-17 11:11:11 +0300

    a

--- routes/product.route.js
+++ routes/product.route.js
@@ -3,7 +3,7 @@ const router = Router();
 const { productController } = require("../controllers/product.controller")
 const authMiddleware = require("../middleware/auth.middleware")
 
-router.post('/product', authMiddleware, productController.addProduct);
+router.post('/product', productController.addProduct);
 router.get('/product', authMiddleware, productController.getProducts);
 router.patch('/product/:id', productController.patchProduct);
 router.delete('/product/:id', productController.deleteProduct);
