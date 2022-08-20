7bad5248eec1678e53eb92273780c5399c8d806d
TAPAEB <aleksejpushkin223@gmail.com>
2022-08-20 18:53:26 +0300

    одернизировал гет запрос на продукты

---
+++ routes/favorite.route.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+const router = Router();
+const { favoriteController } = require('../controllers/favorite.controller');
+
+// router.get('/favorite/:userId', cartController.getCart);
+// router.post('/cart/:userId', cartController.postCart);
+router.post('/favorite/add/:userId',  favoriteController.productAddFavorite);
+// router.patch('/cart/inc/:userId', cartController.productIncCart);
+// router.patch('/cart/dec/:userId', cartController.productDecCart);
+router.delete('/favorite/delete/:userId', favoriteController.productDeleteFavorite)
+// router.delete('/cart/reset/:userId', cartController.productResetCart)
+
+
+module.exports = router;
